049c4ccd04145d78bb197f904916cdb8edd63f8b
brinkerhof <murilocabral8@gmail.com>
2022-06-28 22:35:51 -0300

    bcrypt Installed, create Fixed and update Created

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,8 @@
+const { hash } = require("bcryptjs");
+
 const AppError = require("../utils/AppError");
+
+const sqliteConnection = require("../database/sqlite");
 
 class UsersControllers {
   /**
@@ -8,14 +12,62 @@ class UsersControllers {
    * update - PUT para atualizar um registro
    * delete - DELETE para remover um registro
    */
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body;
 
-    if (!name) {
-      throw new AppError("Name é obrigatorio");
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Esse email ja existe");
     }
 
-    res.status(201).json({ name: name, email: email, password: password });
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    );
+    return res.status(201).json();
+  }
+
+  async update(req, res) {
+    const { name, email } = req.body;
+
+    const { id } = req.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("User do not exists");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
+      throw new AppError("This email already exists");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(
+      `
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ? WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    );
+
+    return res.json();
   }
 }
 
